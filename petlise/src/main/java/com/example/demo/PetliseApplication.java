2adde6eb394f3b1ed58a3183a60b11ba94264e96
SanghunSeo <sh65111595@gmail.com>
2023-06-30 19:53:06 +0900

    MyPage, LiSe Shop 주문,결제 페이지 프론트 1차 완료

--- petlise/src/main/java/com/example/demo/PetliseApplication.java
+++ petlise/src/main/java/com/example/demo/PetliseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "mypage")
+@ComponentScan(basePackages = {"mypage", "shop"})
 
 public class PetliseApplication {
 
