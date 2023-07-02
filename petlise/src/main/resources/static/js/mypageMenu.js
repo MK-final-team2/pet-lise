58a8308521be6b5c9b3b4c008b6bad8d164d2f7e
KKangdaa <97223303+KKangdaa@users.noreply.github.com>
2023-07-02 15:45:04 +0900

    Feature: [#15] 마이페이지 유저소개 페이지 디자인 구현

---
+++ petlise/src/main/resources/static/js/mypageMenu.js
@@ -0,0 +1,44 @@
+document.getElementById('mypageMenu').innerHTML += `
+  <div class="menu_List">
+    <div class="menu_MemberInfo">
+      <div>
+        <span class="material-symbols-outlined">person</span> 회원정보
+      </div>
+      <ul>
+        <li><a href="#">회원정보 수정</a></li>
+        <li><a href="#">회원 탈퇴</a></li>
+      </ul>
+    </div>
+    <div class="menu_Community">
+      <div>
+        <span class="material-symbols-outlined">pets</span> Pet LiSe
+      </div>
+      <ul>
+        <li><a href="#">찜 레시피</a></li>
+        <li><a href="#">좋아요 레시피</a></li>
+        <li><a href="#">내가 쓴 레시피</a></li>
+        <li><a href="#">내가 쓴 게시글</a></li>
+        <li><a href="#">내가 쓴 댓글</a></li>
+      </ul>
+    </div>
+    <div class="menu_Shop">
+      <div>
+        <span class="material-symbols-outlined">shopping_cart</span> Pet
+        LiSe Shop
+      </div>
+      <ul>
+        <li><a href="#">주문목록</a></li>
+        <li><a href="#">내가 쓴 후기</a></li>
+      </ul>
+    </div>
+    <div class="menu_QnA">
+      <div>
+        <span class="material-symbols-outlined">forum</span> 문의사항
+      </div>
+      <ul>
+        <li><a href="#">내가 남긴 문의</a></li>
+        <li><a href="#">상품 문의</a></li>
+      </ul>
+    </div>
+  </div>
+`;
